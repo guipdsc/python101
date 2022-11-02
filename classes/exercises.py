0ff7c790fc2f0a91f34198029f88ab794d1dbae4
Fernando Afonso <Fernando.Afonso@criticaltechworks.com>
2022-11-02 15:29:04 +0000

    all exercises solved

--- classes/exercises.py
+++ classes/exercises.py
@@ -3,8 +3,37 @@ Python 101 - CI Academy 2022
 
 Class exercise
 """
+import math
+class Triangle:
+    sides = 3
 
+    def __init__(self,angle1,angle2,angle3):
+        if angle1+angle2+angle3 != 180:
+            raise ValueError("Not a triangle!")
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
 
+    def __str__(self):
+        return [self.angle1,self.angle2,self.angle3]
+
+    def check_angle(self):
+        return self.angle1+self.angle2+self.angle3 == 180
+    
+    def calc_tri(self,per):
+        cal = math.sin(math.radians(self.angle1))+math.sin(math.radians(self.angle2))+math.sin(math.radians(self.angle3))
+        res = per/cal
+        a = round(math.sin(math.radians(self.angle1))*res,3)
+        b = round(math.sin(math.radians(self.angle2))*res,3)
+        c = round(math.sin(math.radians(self.angle3))*res,3)
+        return[a,b,c]
+
+class Equilateral(Triangle):
+    def __init__(self):
+        super().__init__(60,60,60)
+
+    def area_calc(self,side):
+        return round(((3**0.5)/4)*(side**2),3)
 # 1.1. Create a class, `Triangle`.
 #
 #    Its __init__() method should take `self`, `angle1`, `angle2`, and `angle3` as arguments.
@@ -18,7 +47,7 @@ Class exercise
 #    `self.angle1`, `self.angle2`, and `self.angle3` is equal 180, and False otherwise.
 
 # 1.5. Create a `Triangle` class method that receives the perimeter and calculates the length of each side
-#      The side lengths should be returned in a set.
+#      The side lengths should be returned in a List.
 
 # 2. Create an `Equilateral` class that inherits from `Triangle`
 #
