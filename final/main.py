5cdda5ba28da0a4b4810aa3ef29ce947353e00af
Vitor Luis <vitor.luis92@outlook.com>
2022-11-03 18:17:01 +0000

    added more stuff

--- final/main.py
+++ final/main.py
@@ -11,10 +11,23 @@ def play(player, computer):
     else:
         print(f'You draw!!!')
 
+def instructions():
+    print("Winning Rules of the Rock paper scissor game as follows: \n"
+                                    +"rock vs paper->paper wins \n"
+                                    + "rock vs scissor->rock wins \n"
+                                    +"paper vs scissor->scissor wins \n")
 
 
+def read_input():
+    while True:
+        print("Enter choice \n 0 for rock \n 1 for paper \n 2 for scissor \n 3 for instructions \n 4 for exit ")
 
+        players_choice =  input()
 
+        if players_choice == '0' or players_choice == '1' or players_choice == '2' or players_choice == '3' or players_choice == '4':
+            return int(players_choice)
+        else:
+            print('please choose from one of the valid options')
 
 
 
@@ -23,7 +36,7 @@ computer = Gameplay()
 
 while True:
     print('start:')
-    option = int(input())
+    option = read_input()
 
     computer.set_play_random()
 
@@ -35,3 +48,15 @@ while True:
         instructions()
     else:
         break
+
+'''
+clears
+mostrar a nossa jogada
+
+nomes e ler args
+mostrar pontuacao a cada jogada
+alterar 0 1 2 3 para nomes starts with
+
+funcao q fecha o jogo se score max for atingido
+'''
+
