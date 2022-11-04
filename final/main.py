a49c11e1826137d1a5c7938a1dfdaca627d5aefd
Arnaldo_Alves <arnaldo.alves@criticaltechworks.com>
2022-11-04 15:57:18 +0000

    finishing name functions

--- final/main.py
+++ final/main.py
@@ -6,11 +6,22 @@ from sys import *
 
 
 def get_older_name():
-    #to be done
-    return 'teste'
+
+    try:
+        with open('older_name.txt', 'r') as f:
+            lines = f.readlines()
+            return lines[0]
+    except:
+        with open('older_name.txt', 'w+') as f:
+            return 'default'
+    
+def save_name(name):
+    with open('older_name.txt', 'w+') as f:
+        f.write(name)
 
 def aplly_name(args):
     if args['name'] != False:
+        save_name(args['name'])
         return args['name']
 
     print(f'Before start playing choose your name:')
@@ -18,6 +29,8 @@ def aplly_name(args):
 
     if name == '':
         name = get_older_name()
+
+    save_name(name)
 
     return name
 
